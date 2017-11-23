3ba6029840088a2b400594913c1c05ca6b6256b5
Brad <bradtaniguchi@gmail.com>
2017-11-23 13:59:04 -0800

    Added more to settings page
    - just the basics of the layout, no inputs yet.

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { MatCardModule, MatInputModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+
+    // angular material
+    FlexLayoutModule,
+    MatCardModule,
+    MatInputModule
   ],
   declarations: [SettingsComponent]
 })
